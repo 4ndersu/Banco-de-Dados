f8abe90dfe78b13e33f28edd8a96b952c0223c63
Anderson Vicente <167025546+4ndersu@users.noreply.github.com>
2026-08-24 14:12:51 -0300

    Add SQL queries for student and course data

--- BD2/lista.sql
+++ BD2/lista.sql
@@ -1,1 +1,19 @@
+--q1
+select * from aluno
+where genero = 'Feminino' and anoentrada = 2023;
 
+--q2
+select count(*) from aluno
+where anoentrada = 2022 and codcurso = 1
+
+--q3
+select count(*) from curso
+where ch = 60
+
+--q4
+select * from aluno
+where nome ilike '%Silva' and anoentrada = 2022
+
+--q5
+select count(*) from aluno
+where anoentrada = 2021 and genero = 'Masculino'
