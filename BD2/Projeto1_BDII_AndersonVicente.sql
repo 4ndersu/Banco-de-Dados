7a4443d5009691312db2b9899f1d6bea521bf8a2
Andersu <anderson.vicente@discente.univasf.edu.br>
2026-09-14 13:57:42 -0300

    Tabelas finalizadas(eu acho)

--- BD2/Projeto1_BDII_AndersonVicente.sql
+++ BD2/Projeto1_BDII_AndersonVicente.sql
@@ -1,86 +1,95 @@
---Estúdio: (idEstúdio, nome, paísOrigem, dataFundação, email)
---Obra: (idObra, título, descrição, genero, tipoObra, dataLançamento, classificacaoIndicativa, orçamento, idEstudio)
---Elenco: (idPessoa, nome, nacionalidade, dataNasc)
---Elenco_obra(idPessoa, idObra, funcao, salario, bonus) 
---Plano: (idPlano, tipoPlano, valorMensal, qualidadeMax, maxTelas, exibeanuncios)
---Usuário: (idUsuario, nome, email, senha, idPlano)
---Perfil: (idPerfil, nomePerfil, tipoPerfil, idUsuario)
---Anunciante: (idAnunciante, nomeEmpresa, cnpj, email)
---Anuncio: (idAnuncio, titulo, duracaoSegundos, videoUrl, idAnunciante)
+-- Criando Tipos Personalizados (ENUMs)
+CREATE TYPE genero AS ENUM ('Açao', 'Aventura', 'Comedia', 'Drama', 'Ficcao Cientifica', 'Romance', 'Terror');
+CREATE TYPE tipoObra AS ENUM ('Filme', 'Serie', 'Documentario', 'Animacao');
+CREATE TYPE funcao AS ENUM ('Ator', 'Diretor', 'Roteirista', 'Produtor', 'Fotografo', 'Editor');
+CREATE TYPE tipoPlano AS ENUM ('Basico', 'Padrao', 'Premium');
+CREATE TYPE qualidadeMax AS ENUM ('720p', '1080p', '4K');
+CREATE TYPE maxTelas AS ENUM ('1', '2', '4');
+CREATE TYPE exibeAnuncios AS ENUM ('Sim', 'Nao');
+CREATE TYPE tipoPerfil AS ENUM ('Adulto', 'Infantil', 'Teen', 'Familia');
 
+-- Criando as Tabelas
 CREATE TABLE Estudio ( 
-    idEstudio INT PRIMARY KEY,
+    idEstudio VARCHAR(9) PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    paisOrigem VARCHAR(100) NOT NULL,
+    paisOrigem VARCHAR(45) NOT NULL,
     dataFundacao DATE NOT NULL,
-    email VARCHAR(100) UNIQUE
+    email VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE Obra ( 
-    idObra INT PRIMARY KEY,
+    idObra VARCHAR(9) PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
-    descricao VARCHAR(300) NOT NULL,
-    genero VARCHAR(100) NOT NULL,
-    tipoObra VARCHAR(100) NOT NULL,
+    sinopse VARCHAR(300) NOT NULL,
+    Genero genero NOT NULL,
+    TipoObra tipoObra NOT NULL,
     dataLancamento DATE NOT NULL,
-    classificacaoIndicativa CHAR(2) NOT NULL,
-    orcamento DECIMAL(10,2) NOT NULL,
-    idEstudio INT,
+    classificacaoIndicativa INT NOT NULL CONSTRAINT classificacao_valida CHECK (classificacaoIndicativa BETWEEN 0 AND 18),
+    orcamento DECIMAL(10,2) NOT NULL CONSTRAINT orcamento_positivo CHECK (orcamento > 0.0),
+    idEstudio VARCHAR(9) NOT NULL,
 
     CONSTRAINT fk_estudio FOREIGN KEY (idEstudio) REFERENCES Estudio(idEstudio)
 );
 
 CREATE TABLE Elenco ( 
-    idPessoa INT PRIMARY KEY,
+    idPessoa VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     nacionalidade VARCHAR(30) NOT NULL,
-    dataNasc DATE NOT NULL
+    dataNasc DATE NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE Elenco_Obra ( 
-    idObra INT,
-    idPessoa INT,
-    funcao VARCHAR(30) NOT NULL,
-    salario DECIMAL(10,2) NOT NULL,
-    bonus DECIMAL(10,2),
+    idObra VARCHAR(9),
+    idPessoa VARCHAR(11),
+    Funcao funcao NOT NULL,
+    salario DECIMAL(10,2) NOT NULL DEFAULT 0.0 CONSTRAINT salario_positivo CHECK (salario >= 0.0),
+    bonus DECIMAL(10,2) NOT NULL DEFAULT 0.0 CONSTRAINT bonus_positivo CHECK (bonus >= 0.0),
 
-    CONSTRAINT pk_composta PRIMARY KEY (idObra, idPessoa),
+    CONSTRAINT pk_composta PRIMARY KEY (idObra, idPessoa, Funcao),
     CONSTRAINT fk_obra FOREIGN KEY (idObra) REFERENCES Obra(idObra),
     CONSTRAINT fk_pessoa FOREIGN KEY (idPessoa) REFERENCES Elenco(idPessoa)
 );
 
 CREATE TABLE Plano (
-    idPlano INT PRIMARY KEY,
-    tipoPlano VARCHAR(10) NOT NULL,
+    idPlano VARCHAR(9) PRIMARY KEY,
+    tipoPlano tipoPlano NOT NULL,
     valorMensal DECIMAL(4,2) NOT NULL,
-    qualidadeMax VARCHAR(5) NOT NULL,
-    maxTelas INT NOT NULL,
-    exibeAnuncios CHAR(3) NOT NULL
+    qualidadeMax qualidadeMax NOT NULL,
+    maxTelas maxTelas NOT NULL,
+    exibeAnuncios exibeAnuncios NOT NULL,
+
+    CONSTRAINT chk_regras_plano CHECK (
+        (tipoPlano = 'Basico'  AND valorMensal = 9.99  AND qualidadeMax = '720p'  AND maxTelas = '1' AND exibeAnuncios = 'Sim') OR
+        (tipoPlano = 'Padrao'  AND valorMensal = 19.99 AND qualidadeMax = '1080p' AND maxTelas = '2' AND exibeAnuncios = 'Nao') OR
+        (tipoPlano = 'Premium' AND valorMensal = 29.99 AND qualidadeMax = '4K'    AND maxTelas = '4' AND exibeAnuncios = 'Nao')
+    )
 );
 
 CREATE TABLE Usuario (
-    idUsuario INT PRIMARY KEY,
+    idUsuario VARCHAR(11) PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    telefone VARCHAR(15) NOT NULL UNIQUE,
+    email VARCHAR(100) UNIQUE,
     senha VARCHAR(10) NOT NULL,
-    idPlano INT,
+    idPlano VARCHAR(9),
     
     CONSTRAINT fk_plano FOREIGN KEY (idPlano) REFERENCES Plano(idPlano)
 );
 
 CREATE TABLE Perfil (
-    idPerfil INT PRIMARY KEY,
+    idPerfil VARCHAR(2) PRIMARY KEY,
     nomePerfil VARCHAR(100) NOT NULL,
-    tipoPerfil VARCHAR(100) NOT NULL,
-    idUsuario INT,
+    tipoPerfil tipoPerfil NOT NULL,
+    idUsuario VARCHAR(11),
     
     CONSTRAINT fk_usuario FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
 );
 
 CREATE TABLE Anunciante (
-    idAnunciante INT PRIMARY KEY,
+    CNPJ VARCHAR(14) PRIMARY KEY,
     nomeEmpresa VARCHAR(100) NOT NULL,
-    cnpj VARCHAR(14) NOT NULL UNIQUE,
+    paisSede VARCHAR(56) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE
 );
 
@@ -89,7 +98,7 @@ CREATE TABLE Anuncio (
     titulo VARCHAR(100) NOT NULL,
     duracaoSegundos INT NOT NULL,
     videoUrl VARCHAR(100) NOT NULL UNIQUE,
-    idAnunciante INT,
+    CNPJ VARCHAR(14),
 
-    CONSTRAINT fk_anunciante FOREIGN KEY (idAnunciante) REFERENCES Anunciante(idAnunciante)
+    CONSTRAINT fk_anunciante FOREIGN KEY (CNPJ) REFERENCES Anunciante(CNPJ)
 );
